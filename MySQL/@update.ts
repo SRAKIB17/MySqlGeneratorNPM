19c5383933521784a12e200c23c043088d263366
SRAKIB17 <rakibulssc5@gmail.com>
2023-07-29 16:53:20 +0600

    'dne

--- MySQL/@update.ts
+++ MySQL/@update.ts
@@ -14,11 +14,12 @@ function genUpdateSql(props: {
 
     const updateInfo = Object.entries(update_data)?.map((info: any[]) => {
         const column = info?.[0];
+        const isNumber = typeof info?.[1] == 'number';
+        const value: any = isNumber ? info?.[1] : info?.[1]?.trim();
 
-        const value: any = info?.[1]?.trim();
+        const check = isNumber ? false : value?.indexOf(column) == 0 || value?.lastIndexOf(column) >= (value?.length - column?.length);
+        return (column + '=' + ((isNumber ? false : (value?.match(/[+|-|\/|*]/gi)?.length == 1 && check)) ? value?.toString() : JSON.stringify(value)))
 
-        const check = value?.indexOf(column) == 0 || value?.lastIndexOf(column) >= (value?.length - column?.length);
-        return (column + '=' + ((value?.match(/[+|-|\/|*]/gi)?.length == 1 && check) ? value?.toString() : JSON.stringify(value)))
     })?.join(',');
 
     const s = `UPDATE ${table} SET ${updateInfo}${queryCondition ? " WHERE " + queryCondition + " " : ""}`;
@@ -29,5 +30,6 @@ function genUpdateSql(props: {
         getSyntax
     }
 }
+
 
 export default genUpdateSql
